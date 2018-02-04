5f6dfb0b6ac20cb89b4aaba4177bea55335b146d
Felipe Nicoletto <felipenicoletto@gmail.com>
2018-02-04 18:22:25 -0200

    Packages update

--- Samples/Sample.UWP/MainPage.xaml.cs
+++ Samples/Sample.UWP/MainPage.xaml.cs
@@ -5,7 +5,7 @@
         public MainPage()
         {
             this.InitializeComponent();
-
+            
             Sample.App.DisplayScreenHeight = (float)Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBounds.Height;
             Sample.App.DisplayScreenWidth = (float)Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBounds.Width;
             Sample.App.DisplayScaleFactor = (float)Windows.Graphics.Display.DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
